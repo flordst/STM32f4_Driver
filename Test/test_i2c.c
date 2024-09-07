1a688c8277299c563dca32153ccfe1dfd02395d4
Quy Huynh <103742881+flordst@users.noreply.github.com>
2024-09-07 14:36:58 +0700

    Update test_i2c.c

--- Test/test_i2c.c
+++ Test/test_i2c.c
@@ -10,7 +10,6 @@
 int main(void)
 {
 
-/*	HAL_Init();*/
 	OLED_SSD1306_Init();
 	OLED_SSD1306_Puts("HELLO WORLD ",&OLED_Font_11x18, OLED_COLOR_WHITE);
 	OLED_SSD1306_UpdateScreen(); // Always update screen after any changes to appear in OLED
